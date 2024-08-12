eba4586b0087ab115a76f56ae713ae5e77aa50e1
Muhammad Amirul <mirulid@gmail.com>
2024-08-12 08:17:26 +0700

    add search input

--- App.js
+++ App.js
@@ -1,20 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import {
+  Button,
+  XStack,
+  YStack,
+  Text,
+  Input,
+  Label,
+  Checkbox,
+  ScrollView,
+  Image
+} from "tamagui";
+import { TamaguiProvider, createTamagui } from "@tamagui/core";
+import { config } from "@tamagui/config/v3";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+import { Check as CheckIcon } from "@tamagui/lucide-icons";
+import { jobs } from "./Seeder.ts";
 
+SplashScreen.preventAutoHideAsync();
+const tamaguiConfig = createTamagui(config);
 export default function App() {
+  const [loaded, error] = useFonts({
+    cyber: require("./assets/fonts/cyber.ttf"),
+    toleran: require("./assets/fonts/Tolerant.ttf"),
+    "poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={tamaguiConfig}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <XStack
+            backgroundColor="skyblue"
+            width="100%"
+            marginTop={32}
+            height={60}
+            alignItems="center"
+            justifyContent="space-between"
+            paddingHorizontal={10}
+            position="absolute"
+            top={0}
+            zIndex={1000}
+          >
+            <Text fontSize={32} fontWeight={500} fontFamily={"poppins-Bold"}>
+              GitHub Job
+            </Text>
+            <Button size="$4" circular backgroundColor={"transparent"}>
+              <AntDesign name="search1" size={24} color="black" />
+            </Button>
+          </XStack>
+
+          <YStack marginTop={100}>
+            <Label>Location</Label>
+            <Input size="$4" width={300}></Input>
+
+            <Label>Description</Label>
+            <Input size="$4" width={300}></Input>
+            <XStack alignItems="center">
+              <Checkbox size="$4">
+                <Checkbox.Indicator>
+                  <CheckIcon />
+                </Checkbox.Indicator>
+              </Checkbox>
+              <Label padding={5}>Fulltime</Label>
+            </XStack>
+            {jobs.map((data) => {
+              return (
+                <XStack height={100} backgroundColor="gray">
+                  <YStack>
+                  <Label >{data.title}</Label>
+                  <Label >{data.company}</Label>
+
+                  </YStack>
+                  <Image
+      source={{
+        uri: data.company_logo,
+        width: 200,
+        height: 300,
+      }}
+    />
+                </XStack>
+              );
+            })}
+          </YStack>
+        </ScrollView>
+      </SafeAreaView>
+    </TamaguiProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
